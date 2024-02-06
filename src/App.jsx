601a6955cfbd103d6139baccc3692947108b9186
Emilse Arias <emilsearias@gmail.com>
2024-02-06 11:17:43 -0300

    modificamos la orientacion de las opciones del menú

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        {/* <Route exact path="/administrador/crear" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/editar" element={<Administrador></Administrador>}></Route> */}
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
       <Footer></Footer>
