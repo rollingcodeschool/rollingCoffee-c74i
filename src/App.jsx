0033c0a73c8d1fad743c10d4fabd9dac4bbbe6f8
Emilse Arias <emilsearias@gmail.com>
2024-02-06 10:39:42 -0300

    agregamos rutas en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/common/Footer'
-import Menu from './components/common/Menu'
-import Administrador from './components/pages/Administrador'
-import Inicio from './components/pages/Inicio'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/common/Footer";
+import Menu from "./components/common/Menu";
+import Administrador from "./components/pages/Administrador";
+import Inicio from "./components/pages/Inicio";
+import Error404 from "./components/pages/Error404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
- 
-
   return (
-    <>
-    <Menu></Menu>
-    <Inicio></Inicio>
-    {/* <Administrador></Administrador> */}
-    <Footer></Footer>
-    </>
-  )
+    //administrador de rutas
+    // www.rollingCoffee.com/
+    <BrowserRouter>
+      <Menu></Menu>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
