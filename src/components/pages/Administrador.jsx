ca94e0afd92b5d20bb69de3f8ccd054de7c19504
Emilse Arias <emilsearias@gmail.com>
2024-02-08 11:39:00 -0300

    agregamos validaciones con react-hook-form en el input de nombreProducto

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,33 +1,36 @@
 import { Container, Table, Button } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
+import { Link } from "react-router-dom";
 const Administrador = () => {
+
+
   return (
     <Container className="mainContainer">
-       <div className="d-flex justify-content-between align-items-center mt-5">
+      <div className="d-flex justify-content-between align-items-center mt-5">
         <h1 className="display-4 ">Productos disponibles</h1>
-        <button className="btn btn-primary" to="/administrador/crear">
-        <i className="bi bi-file-earmark-plus"></i>
-        </button>
+        <Link className="btn btn-primary" to="/administrador/crear">
+          <i className="bi bi-file-earmark-plus"></i>
+        </Link>
       </div>
       <div className="table-responsive">
-      <Table responsive striped bordered hover>
-        <thead>
-          <tr className="text-center">
-            <th>Cod</th>
-            <th>Producto</th>
-            <th>Precio</th>
-            <th>URL de Imagen</th>
-            <th>Categoria</th>
-            <th>Opciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-        </tbody>
-      </Table>
+        <Table responsive striped bordered hover>
+          <thead>
+            <tr className="text-center">
+              <th>Cod</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>URL de Imagen</th>
+              <th>Categoria</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+          </tbody>
+        </Table>
       </div>
     </Container>
   );
--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,33 +1,52 @@
 import { Form, Button } from "react-bootstrap";
-
+import { useForm } from "react-hook-form";
 
 const FormularioProducto = () => {
+const {register, handleSubmit, formState:{errors}} = useForm();
+
+const onSubmit = (producto)=>{
+  console.log(producto);
+}
+
 
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Nuevo producto</h1>
       <hr />
-      {/* <Form onSubmit={handleSubmit}> */}
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ej: Cafe"
-          />
-          <Form.Text className="text-danger">
-            prueba de error
-          </Form.Text>
+          <Form.Control type="text" placeholder="Ej: Cafe" {
+            ...register("nombreProducto",{
+              required: "El nombre del producto es obligatorio",
+              minLength:{
+                value:2,
+                message: "Debe ingresar como minimo 2 caracteres"
+              },
+              maxLength:{
+                value:30,
+                message: "Debe ingresar como maximo 30 caracteres"
+              }
+            })
+          } />
+          <Form.Text className="text-danger">{errors.nombreProducto?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Ej: 50"
-          />
-          <Form.Text className="text-danger">
-          prueba de error
-          </Form.Text>
+          <Form.Control type="number" placeholder="Ej: 50" {
+            ...register("precio",{
+              required: "El precio es obligatorio",
+              min:{
+                value:2,
+                message: "Debe ingresar como minimo 2 caracteres"
+              },
+              max:{
+                value:30,
+                message: "Debe ingresar como maximo 30 caracteres"
+              }
+            })
+          } />
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
@@ -35,9 +54,7 @@ const FormularioProducto = () => {
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
           />
-          <Form.Text className="text-danger">
-        prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
@@ -48,9 +65,7 @@ const FormularioProducto = () => {
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger">
-          prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción breve*</Form.Label>
@@ -59,9 +74,7 @@ const FormularioProducto = () => {
             placeholder="Ej: Una taza de café suave y aromático."
             as="textarea"
           />
-          <Form.Text className="text-danger">
-        prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción Amplia*</Form.Label>
@@ -70,12 +83,10 @@ const FormularioProducto = () => {
             placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
             as="textarea"
           />
-          <Form.Text className="text-danger">
-        prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
-        
-        <Button type="submit" variant='success'>
+
+        <Button type="submit" variant="success">
           Guardar
         </Button>
       </Form>
