b0f1a28b0642774f8cb8bb60029ca93ce72d11ca
Emilse Arias <emilsearias@gmail.com>
2024-02-15 10:06:43 -0300

    agregamos las validaciones del formularioProducto

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -2,12 +2,15 @@ import { Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const FormularioProducto = () => {
-const {register, handleSubmit, formState:{errors}} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-const onSubmit = (producto)=>{
-  console.log(producto);
-}
-
+  const onSubmit = (producto) => {
+    console.log(producto);
+  };
 
   return (
     <section className="container mainSection">
@@ -16,56 +19,78 @@ const onSubmit = (producto)=>{
       <Form className="my-4" onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
-          <Form.Control type="text" placeholder="Ej: Cafe" {
-            ...register("nombreProducto",{
+          <Form.Control
+            type="text"
+            placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
               required: "El nombre del producto es obligatorio",
-              minLength:{
-                value:2,
-                message: "Debe ingresar como minimo 2 caracteres"
+              minLength: {
+                value: 2,
+                message: "Debe ingresar como minimo 2 caracteres",
               },
-              maxLength:{
-                value:30,
-                message: "Debe ingresar como maximo 30 caracteres"
-              }
-            })
-          } />
-          <Form.Text className="text-danger">{errors.nombreProducto?.message}</Form.Text>
+              maxLength: {
+                value: 30,
+                message: "Debe ingresar como maximo 30 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="Ej: 50" {
-            ...register("precio",{
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
               required: "El precio es obligatorio",
-              min:{
-                value:2,
-                message: "Debe ingresar como minimo 2 caracteres"
+              min: {
+                value: 100,
+                message: "Debe ingresar como un monto de $100",
               },
-              max:{
-                value:30,
-                message: "Debe ingresar como maximo 30 caracteres"
-              }
-            })
-          } />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+              max: {
+                value: 10000,
+                message: "Debe ingresar como maximo el monto es de $10000",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
+            {...register("imagen", {
+              required: "La imagen es obligatorio",
+              pattern: {
+                value: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/i,
+                message:
+                  "Debe ingresar una URL de imagen valida (png|jpg|jpeg|gif|png|svg)",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select>
+          <Form.Select
+            {...register("categoria", {
+              required: "La categoria es obligatoria",
+            })}
+          >
             <option value="">Seleccione una opcion</option>
             <option value="Infusiones">Infusiones</option>
             <option value="Batidos">Batidos</option>
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">{errors.categoria?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción breve*</Form.Label>
@@ -73,8 +98,19 @@ const onSubmit = (producto)=>{
             type="text"
             placeholder="Ej: Una taza de café suave y aromático."
             as="textarea"
+            {...register("descripcion_breve", {
+              required: "La descripcion breve es obligatoria",
+              minLength: {
+                value: 10,
+                message: "Debe ingresar como minimo 10 caracteres",
+              },
+              maxLength: {
+                value: 100,
+                message: "Debe ingresar como maximo 100 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">{errors.descripcion_breve?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción Amplia*</Form.Label>
@@ -82,8 +118,19 @@ const onSubmit = (producto)=>{
             type="text"
             placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
             as="textarea"
+            {...register("descripcion_amplia", {
+              required: "La descripcion amplia es obligatoria",
+              minLength: {
+                value: 20,
+                message: "Debe ingresar como minimo 20 caracteres",
+              },
+              maxLength: {
+                value: 1000,
+                message: "Debe ingresar como maximo 1000 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">{errors.descripcion_amplia?.message}</Form.Text>
         </Form.Group>
 
         <Button type="submit" variant="success">
