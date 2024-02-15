02659befcc6e5a16b90f55a6e548e34a1134e1b7
Emilse Arias <emilsearias@gmail.com>
2024-02-15 11:13:16 -0300

    agregamos la funcion crearProductoAPI

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,3 +2,19 @@ const URI_Producto = import.meta.env.VITE_API_PRODUCTO;
 
 console.log(URI_Producto);
 
+// POST
+export const crearProductoAPI = async (productoNuevo) => {
+  try {
+    const respuesta = await fetch(URI_Producto, {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(productoNuevo)
+    });
+    console.log(respuesta)
+    return respuesta
+  } catch (error) {
+    console.log(error);
+  }
+};
