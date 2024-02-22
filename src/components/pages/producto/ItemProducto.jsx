26c26fe56ef56008b13ddf300dc0bd6f6d28b6bc
Emilse Arias <emilsearias@gmail.com>
2024-02-22 09:47:42 -0300

    agregamos un mensaje cuando vamos a eliminar un producto

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,5 +1,28 @@
 import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 const ItemProducto = ({producto}) => {
+
+  const borrarProducto = ()=>{
+    Swal.fire({
+      title: "¿Esta seguro de eliminar el producto?",
+      text: "No se puede revertir este paso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Producto eliminado",
+          text: `El producto "${producto.nombreProducto}" fue eliminado correctamente`,
+          icon: "success"
+        });
+      }
+    });
+  }
+
   return (
     <tr>
       <td className="text-center">{producto.id}</td>
@@ -17,7 +40,7 @@ const ItemProducto = ({producto}) => {
         <Button variant="warning" className="me-lg-2">
           <i className="bi bi-pencil-square"></i>
         </Button>
-        <Button variant="danger">
+        <Button variant="danger" onClick={borrarProducto}>
           <i className="bi bi-trash"></i>
         </Button>
       </td>
